8c19d13302b1e662adaffde20c05f523b2b2fd42
Draidoldidit <switchhmgmt@gmail.com>
2026-01-04 13:13:00 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,29 +1,8 @@
-let ITEM_DATA = [];
-
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("data-template.csv")
-    .then(res => res.text())
-    .then(text => parseCSV(text));
-});
-
-function parseCSV(text) {
-  const lines = text.split("\n").slice(1);
-  ITEM_DATA = lines.map(line => {
-    const [Profession, Item, Rarity, Materials] = line.split(",");
-    return { Profession, Item, Rarity, Materials };
-  }).filter(i => i.Profession);
-
-  populateProfessions();
-}
-
-function populateProfessions() {
-  const select = document.getElementById("professionSelect");
-  [...new Set(ITEM_DATA.map(i => i.Profession))].forEach(p => {
-    const opt = document.createElement("option");
-    opt.value = p;
-    opt.textContent = p;
-    select.appendChild(opt);
-  });
-
-  select.addEventListener("change", showItemsForProfession);
-}
+const ITEM_DATA = [
+  { Profession: "Weaponsmithing", Item: "Iron Sword", Rarity: "Common", Material: "Iron Ore", Quantity: 3 },
+  { Profession: "Weaponsmithing", Item: "Iron Sword", Rarity: "Common", Material: "Wood", Quantity: 1 },
+  { Profession: "Weaponsmithing", Item: "Steel Warhammer", Rarity: "Rare", Material: "Steel Ingot", Quantity: 5 },
+  { Profession: "Weaponsmithing", Item: "Steel Warhammer", Rarity: "Rare", Material: "Oak Wood", Quantity: 2 },
+  { Profession: "Alchemy", Item: "Minor Health Potion", Rarity: "Common", Material: "Herb", Quantity: 2 },
+  { Profession: "Alchemy", Item: "Minor Health Potion", Rarity: "Common", Material: "Water Flask", Quantity: 1 }
+];
